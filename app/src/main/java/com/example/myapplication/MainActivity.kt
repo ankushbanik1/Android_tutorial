436490a251942a39cbd6ed6d1f47e6e23c9ec055
ankush banik <ankushbanik1234@gmail.com>
2019-02-09 16:22:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        enter1.setOnClickListener { Log.i("key","hello my app") }
+
+
+    }
+}
